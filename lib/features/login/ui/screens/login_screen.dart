dd1a25f2f6ac707ff802a9d48bc1f111cd8d63e0
devleen <aatya210@gmail.com>
2024-03-31 01:25:27 +0200

    add text to login page

--- lib/features/login/ui/screens/login_screen.dart
+++ lib/features/login/ui/screens/login_screen.dart
@@ -1,4 +1,7 @@
+import 'package:doctors_app/core/helpers/spacing.dart';
+import 'package:doctors_app/core/theming/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,8 +9,29 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('LoginScreen'),),
-      body: const SizedBox.shrink(),
+      appBar: AppBar(
+        title: const Text('LoginScreen'),
+      ),
+      body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+            child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome Back',
+                style: TextStyleManager.font20BlueBold,
+              ),
+              verticalSpace(10),
+              Text(
+                "We're excited to have you back, can't wait to see what you've been up to since you last logged in.",
+                style: TextStyleManager.font16Back500W,
+              )
+            ],
+                    ),
+                  ),
+          )),
     );
   }
 }
